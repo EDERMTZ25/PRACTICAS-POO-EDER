ed7348ebda39aac63753a2b91ca23e22920aabbf
EDERMTZ25 <123896637+EDERMTZ25@users.noreply.github.com>
2023-02-22 15:45:46 -0600

    Aqui se de
    Aqui se creo unq clase con el nombre personaje ademas se definieron sus caracteristicas y atributos

---
+++ Objetos.py
@@ -0,0 +1,17 @@
+import Personaje 
+
+#1. Crear un objeto de la clase Personaje
+heroe = Personaje()
+
+#2. Usar atributos del objeto
+
+print("El personaje se llama: "+ heroe.nombre)
+print("El personaje es de la especie: "+ heroe.especie)
+print("El personaje mide: "+ heroe.altura+" metros")
+
+#3. Usar metodos del objeto
+heroe.correr(True)
+heroe.lanzarGranadas()
+heroe.recargarArma(87)
+
+#Fin del programa
