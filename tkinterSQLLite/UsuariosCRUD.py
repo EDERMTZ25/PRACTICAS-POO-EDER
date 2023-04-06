07ad66efc19b185dc39810afe0049bdfe4d5e787
EDERMTZ25 <123896637+EDERMTZ25@users.noreply.github.com>
2023-04-06 06:44:15 -0500

    En esta parte se creo una interfaz para modificar y eliminar usuario

--- tkinterSQLLite/UsuariosCRUD.py
+++ tkinterSQLLite/UsuariosCRUD.py
@@ -1,83 +1,211 @@
-from tkinter import*
-from tkinter import ttk
 import tkinter as tk
-from controladorBD import*
+from tkinter import ttk, END
 from tkinter import messagebox
+from ControladorBD import *
 
-#crear un objeto tipo controlador
-controlador= controladorBD()
+#. Crear una objeto de tipo contralador
+controlador= ControladorBD()
 
-#proceder a guadar el metodo guardarUsuario() del objeto controlador
-def ejecutarInsert():
-    controlador.guardarUsuario(varNom.get(),varCor.get(),varCon())
+#Procedermos a guardar usando el metodo del objeto controlador.
+def ejecutaInsert():
+    controlador.guardarUsuario(varNombre.get(),varCorreo.get(),varContra.get())
 
 
-def ejecutaSelectU():
-    rsUsuario=controlador.consultarUsuario(varbus.get())
-    for usu in rsUsuario:
-        cadena=str(usu[0])+" "+ usu[1]+ " "+ usu[2]+ " "+ str(usu[3])
-        
-    if(rsUsuario):
-        print(cadena)
+# Funcion para buscar a un usuario con id y ponerlo en el cuadro de text
+def buscar():
+    busqueda = controlador.consultarUsuario(varId.get())
+    txtMostrar.delete('0.0',END)
+
+    # Damos formato a la busqueda para mostrarla en el cuadro de texto (Solo se muestra el id, nombre y correo)
+    if busqueda != None:
+        for usu in busqueda:
+            cadena2 = (" | ID: " + str(usu[0]) + " | Nombre: " + usu[1] + " | Correo: " + usu[2] + " | ")
+
+        txtMostrar.insert('0.0', cadena2)
     else:
-        messagebox.showinfo("Usuario no registrado en la base de datos")
+        pass
 
 
 
-ventana = Tk()
-ventana.title("CRUD Usuarios")
-ventana.geometry("500x300")
 
-panel= ttk.Notebook(ventana)
+# Funcion para traer todos los usuarios
+def mostrar_todos():
+    #Borrar datos del treeview anterior
+    tree.delete(*tree.get_children())
+
+    # Agregar todos los usuarios a un treeview
+    rsUsuarios = controlador.consultarTodos()
+
+    #Valida que haya datos en la variable rsUsuarios
+    if rsUsuarios != None:
+        for usu2 in rsUsuarios:
+            tree.insert(parent='',index='end',values=(usu2[0],usu2[1],usu2[2]))
+    else:
+        pass
+
+# Funcion para actualizar un usuario
+def actualizar():
+    controlador.actualizarUsuario(varId_actu.get(), varNombre_actu.get(), varCorreo_actu.get(), varContra_actu.get())
+
+# Funcion para eliminar un usuario
+def eliminar():
+    if varConfirmar.get() == 1:
+        controlador.eliminarUsuario(varId_elim.get())
+        # Limpiar el campo de texto de eliminar usuario y el checkbutton
+        varConfirmar.set(0)
+        varId_elim.set("")
+    else:
+        messagebox.showwarning("CUIDADO", "Debe confirmar la eliminacion")
+
+
+# Funcion para limpiar los campos de texto de ingresar usuario y actualizar usuario
+def limpiar_ingresar():
+    varNombre.set("")
+    varCorreo.set("")
+    varContra.set("")
+
+
+def limpiar_actualizar():
+    varId_actu.set("")
+    varNombre_actu.set("")
+    varCorreo_actu.set("")
+    varContra_actu.set("")
+
+#Creacion de la ventana principal (CRUD)
+
+Ventana = tk.Tk()
+Ventana.title("CRUD")
+Ventana.geometry("800x600")
+
+#Creacion de las pestañas
+#Panel de pestañas Notebook
+panel = ttk.Notebook(Ventana)
 panel.pack(fill="both", expand="yes")
 
-pestana1=ttk.Frame(panel)
-pestana2=ttk.Frame(panel)
-pestana3=ttk.Frame(panel)
-pestana4=ttk.Frame(panel)
+#Creacion de las pestañas
+#Pestaña 1
+pestana1 = ttk.Frame(panel)
+panel.add(pestana1, text="Ingresar Usuario")
 
-#pestaña 1
-titulo= Label(pestana1, text="Resgistrp de Usurios", fg="blue", font=("Modern",18) ).pack()
-varNom= tk.StringVar()
-lblNom= Label(pestana1, text="nombre: ").pack()
-txtNom= Entry(pestana1, textvariable=varNom ).pack()
+# Agregar los widgets a la pestaña de ingresar usuario y darle formato
+titulo_p1 = tk.Label(pestana1, text="Ingresar Usuario", fg="green", font=("Arial", 20)).pack()
 
-varCor= tk.StringVar()
-lblCor= Label(pestana1, text="correo: ").pack()
-txtcor= Entry(pestana1, textvariable=varCor ).pack()
-
-varCon= tk.StringVar()
-lblCon= Label(pestana1, text="contraseña: ").pack()
-txtcon= Entry(pestana1, textvariable=varCon ).pack()
-
-btnGuardar= Button(pestana1, text="Guardar Usuario").pack()
+# Crear los campos de texto para ingresar usuario
+nombre = tk.Label(pestana1, text="Nombre: ", font=("Arial", 12)).pack(padx=10, pady=10)
+varNombre = tk.StringVar() #Variable para guardar el nombre
+txtNombre = tk.Entry(pestana1, textvariable=varNombre, font=("Arial", 12)).pack(padx=5, pady=5)
 
 
-#pestaña 2 Buscar Usuario
-titulo2= Label(pestana2, text="Buscar Usurios", fg="green", font=("Modern",18) ).pack()
+correo = tk.Label(pestana1, text="Correo: ", font=("Arial", 12)).pack(padx=10, pady=10)
+varCorreo = tk.StringVar() #Variable para guardar el correo
+txtCorreo = tk.Entry(pestana1, textvariable=varCorreo, font=("Arial", 12)).pack(padx=5, pady=5)
 
-varbus= tk.StringVar()
-lblid= Label(pestana2, text="Identificador de usuario: ").pack()
-txtid= Entry(pestana2, textvariable=varbus ).pack()
-btnBusqueda= Button(pestana2, text="Buscar",command=ejecutaSelectU).pack()
+contra = tk.Label(pestana1, text="Contraseña: ", font=("Arial", 12)).pack(padx=10, pady=10)
+varContra = tk.StringVar() #Variable para guardar la contraseña
+txtContra = tk.Entry(pestana1, textvariable=varContra, font=("Arial", 12)).pack(padx=5, pady=5)
 
-subBus= Label(pestana2,text="Registrado: ",fg="blue",font=("Modern",15)).pack()
-texBus=tk.Text(pestana2,height=5,width=52).pack()
+# Crear el boton para ingresar usuario y darle formato
+btnIngresar = tk.Button(pestana1, text="Registrar usuario", font=("Arial", 12), bg="#18BBBB", fg="white", command=ejecutaInsert).pack(padx=10, pady=10)
 
-
-
-
-
+# Crear el boton para limpiar los campos de texto
+btnLimpiar = tk.Button(pestana1, text="Limpiar campos", font=("Arial", 12), bg="#16BF72", fg="white", command=limpiar_ingresar).pack(padx=10, pady=10)
 
 
+#Pestaña 2
+pestana2 = ttk.Frame(panel)
+panel.add(pestana2, text="Buscar Usuario")
 
-panel.add(pestana1, text="Formulario de Usuarios")
-panel.add(pestana2, text="Buscar Usuarios")
-panel.add(pestana3, text="Consultar Usuario")
-panel.add(pestana4, text="Actualiar Usuario")
+# Agregar los widgets a la pestaña de buscar usuario y darle formato
+titulo_p2 = tk.Label(pestana2, text="Buscar Usuario", fg="blue", font=("Arial", 20)).pack()
+
+# Crear los campos de texto para buscar usuario
+id = tk.Label(pestana2, text="ID: ", font=("Arial", 12)).pack(padx=10, pady=10)
+varId = tk.StringVar() #Variable para guardar el id
+txtId = tk.Entry(pestana2, textvariable=varId, font=("Arial", 12)).pack(padx=5, pady=5)
+
+# Crear el boton para buscar usuario y darle formato
+btnBuscar = tk.Button(pestana2, text="Buscar usuario", font=("Arial", 12), bg="#18BBBB", fg="white", command=buscar).pack(padx=10, pady=10)
+
+#Agregar un Textbox para mostrar los datos del usuario
+titulo_texto = tk.Label(pestana2, text="Datos del usuario", font=("Arial", 12)).pack(padx=10, pady=10)
+txtMostrar = tk.Text(pestana2, width=50, height=10, font=("Arial", 12))
+txtMostrar.pack(padx=10, pady=10)
 
 
 
+#Pestaña 3
+pestana3 = ttk.Frame(panel)
+panel.add(pestana3, text="Mostar Todos")
+
+# Agregar los widgets a la pestaña de mostrar todos los usuarios y darle formato
+titulo_p3 = tk.Label(pestana3, text="Mostrar Todos", fg="#14A6DC", font=("Arial", 20)).pack()
+
+# Crear el boton para mostrar todos los usuarios y darle formato
+btnMostrar = tk.Button(pestana3, text="Mostrar todos", font=("Arial", 12), bg="#18BBBB", fg="white", command=mostrar_todos).pack(padx=10, pady=10)
+
+# Usar un Treeview para mostrar todos los usuarios
+titulo_tree = tk.Label(pestana3, text="Todos los usuarios", font=("Arial", 12)).pack(padx=10, pady=10)
+tree = ttk.Treeview(pestana3, columns=(1,2,3), show="headings", height="6")
+tree.pack(padx=10, pady=10)
+tree.heading(1, text="ID")
+tree.heading(2, text="Nombre")
+tree.heading(3, text="Correo")
+
+tree.pack(padx=10, pady=10)
 
 
-ventana.mainloop()
+#Pestaña 4
+pestana4 = ttk.Frame(panel)
+panel.add(pestana4, text="Actualizar Usuario")
+
+# Agregar los widgets a la pestaña de actualizar usuario y darle formato
+titulo_p4 = tk.Label(pestana4, text="Actualizar Usuario", fg="#F19510", font=("Arial", 20)).pack()
+
+# Crear los campos de texto para actualizar usuario
+id = tk.Label(pestana4, text="ID: ", font=("Arial", 12)).pack(padx=10, pady=10)
+varId_actu = tk.StringVar() #Variable para guardar el id
+txtId_actu = tk.Entry(pestana4, textvariable=varId_actu, font=("Arial", 12)).pack(padx=5, pady=5)
+
+nombre_actu = tk.Label(pestana4, text="Nombre: ", font=("Arial", 12)).pack(padx=10, pady=10)
+varNombre_actu = tk.StringVar() #Variable para guardar el nombre
+txtNombre_actu = tk.Entry(pestana4, textvariable=varNombre_actu, font=("Arial", 12)).pack(padx=5, pady=5)
+
+correo_actu = tk.Label(pestana4, text="Correo: ", font=("Arial", 12)).pack(padx=10, pady=10)
+varCorreo_actu = tk.StringVar() #Variable para guardar el correo
+txtCorreo_actu = tk.Entry(pestana4, textvariable=varCorreo_actu, font=("Arial", 12)).pack(padx=5, pady=5)
+
+contra_actu = tk.Label(pestana4, text="Contraseña nueva: ", font=("Arial", 12)).pack(padx=10, pady=10)
+varContra_actu = tk.StringVar() #Variable para guardar la contraseña
+txtContra_actu = tk.Entry(pestana4, textvariable=varContra_actu, font=("Arial", 12)).pack(padx=5, pady=5)
+
+# Crear el boton para actualizar usuario y darle formato
+btnActualizar = tk.Button(pestana4, text="Actualizar usuario", font=("Arial", 12), bg="#18BBBB", fg="white", command=actualizar).pack(padx=10, pady=10)
+
+# Crear el boton para limpiar los campos de texto
+btnLimpiar_actu = tk.Button(pestana4, text="Limpiar campos", font=("Arial", 12), bg="#16BF72", fg="white", command=limpiar_actualizar).pack(padx=10, pady=10)
+
+
+#Pestaña 5
+pestana5 = ttk.Frame(panel)
+panel.add(pestana5, text="Eliminar Usuario")
+
+# Agregar los widgets a la pestaña de eliminar usuario y darle formato
+titulo_p5 = tk.Label(pestana5, text="Eliminar Usuario", fg="red", font=("Arial", 20)).pack()
+
+# Crear los campos de texto para eliminar usuario
+id = tk.Label(pestana5, text="ID: ", font=("Arial", 12)).pack(padx=10, pady=10)
+varId_elim = tk.StringVar() #Variable para guardar el id
+txtId_elim = tk.Entry(pestana5, textvariable=varId_elim, font=("Arial", 12)).pack(padx=5, pady=5)
+
+# Agregar un checkbox para confirmar la eliminacion
+varConfirmar = tk.IntVar()
+confirmar = tk.Checkbutton(pestana5, text="Confirmar", variable=varConfirmar, font=("Arial", 12)).pack(padx=10, pady=10)
+
+# Crear el boton para eliminar usuario y darle formato
+btnEliminar = tk.Button(pestana5, text="Eliminar usuario", font=("Arial", 12), bg="red", fg="white", command=eliminar).pack(padx=10, pady=10)
+
+
+#Ejecucion de la ventana
+Ventana.mainloop()
+
+
--- tkinterSQLLite/controladorBD.py
+++ tkinterSQLLite/controladorBD.py
@@ -1,79 +1,161 @@
+import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 import sqlite3
 import bcrypt
 
 
-
-
-#preparamos conexion
-class controladorBD:
-    
-    def _init_(self):
+class ControladorBD:
+    def __init__(self):
         pass
-    
-    
+
     def conexionBD(self):
-        
         try:
-            conexion=sqlite3.connect("C:\Users\HP\Documents\GitHub\PRACTICAS-POO-EDER\tkinterSQLLite\TBagregados.db")
-            print("Conectado BD")
+            conexion = sqlite3.connect("usuarios_bd.db")
+        # print("Conectado con exito")
             return conexion
         except sqlite3.OperationalError:
-            print("no se puede conectar")
-    
-    
-    
-    def guardarUsuario(self,nom,cor,con):
-        
-        #llamar a la conexion
-        conx= self.conexionBD()
-        
-        #revisar parametros vacios
-        if(nom == "" or cor=="" or con == ""):
-            messagebox.showwarning("aguas!, Revisa tu formulario")
+            print("Fallo en la conexion")
+
+    def encriptarContra(self, contra):
+        # Encritar contraseña con bcrypt
+        contraPlana = contra
+        contraPlana = contraPlana.encode()  # Convertir contraseña a Bytes
+        sal = bcrypt.gensalt()
+        contraEncriptada = bcrypt.hashpw(contraPlana, sal)
+        return contraEncriptada
+
+    def guardarUsuario(self, nombre, correo, contra):
+        conx = self.conexionBD()
+
+        if (nombre == "" or correo == "" or contra == ""):
+            messagebox.showwarning("CUIDADO", "Revisa tus datos, uno o mas campos estan vacios")
             conx.close()
+            return
+        elif (nombre == "" and correo == "" and contra == ""):
+            messagebox.showwarning("CUIDADO", "Todos los campos estan vacios")
+            conx.close()
+            return
         else:
-            #preparar datos de sql
-            cursor= conx.cursor()
-            conH= self.encriptarCon(con)
-            datos=(nom,cor,conH)
-            qrInser="insert into TBagregados(Nombre,Correo,Contraseña) values(?,?,?)"
-            
-            #proceder a insertar y cerramos la conexion
-            cursor.execute(qrInser, datos)
+            cursor = conx.cursor()
+            conH = self.encriptarContra(contra)
+            datos = (nombre, correo, conH)
+            consulta = "insert into TBRegistros(nombre, correo, contra) values(?,?,?)"
+            # print(nombre, correo, contra)
+            cursor.execute(consulta, datos)
             conx.commit()
-            conx.close
-            messagebox.showinfo("Exito"," Se guardo el Usuario")
-            
-    def encriptarCon(self,con):
-        conplana= con
-        conplana= conplana.encode()
-        sal= bcrypt.gensalt()
-        conHa=bcrypt.hashpw(conplana,sal)
-        print(conHa)
-        
-        return conHa
-        
-        
-    def consultarUsuario(self,id):
-        #1.preparar conexion
-        conx=self.conexionBD()
-            
-        #2.verificar que el ID no este vacio
-        if( id == ""):
-            messagebox.showwarning("cuidado","ID vacio escribe uno valido")
-        else:
-            #proceder a buscar
-            try:
-            #preparar lo necesario para el select
-                cursor= conx.cursor()
-                sqlSelect="select * From TbRegistrados where id=" + id
-                
-                cursor.execute(sqlSelect)
-                RSusuario= cursor.fetchall()
-                conx.close()
-                
-                return RSusuario
-                
-            except sqlite3.OperationalError:
-                print("Error Consulta")
+            conx.close()
+            messagebox.showinfo("EXITO", "Se guardo el usuario")
+
+    def consultarUsuario(self, id):
+        # Buscar usuario en la base de datos por su id
+        conx = self.conexionBD()
+        cursor = conx.cursor()
+        try:
+            cursor.execute("select * from TBRegistros where id = ?", (id,))
+            usuario = cursor.fetchall()
+            conx.close()
+
+            # Comprobar si el usuario existe
+            if len(usuario) == 0:
+                # Si el usuario no existe se retorna None y se muestra un mensaje de error
+                messagebox.showwarning("Error", "El usuario no existe")
+                return None
+            else:
+                # Si el usuario existe se retorna el usuario encontrado
+                return usuario
+        # Si ocurre un error al consultar el usuario se muestra un mensaje de error
+        except sqlite3.OperationalError:
+            messagebox.showwarning("Error", "Ocurrio un error al consultar el usuario")
+
+    def consultarTodos(self):
+        # Buscar usuario en la base de datos por su id
+        conx = self.conexionBD()
+        cursor = conx.cursor()
+        try:
+            cursor.execute("select id, nombre, correo from TBRegistros")
+            usuario = cursor.fetchall()
+            conx.close()
+
+            # Comprobar si el usuario existe
+            if len(usuario) == 0:
+                # Si el usuario no existe se retorna None y se muestra un mensaje de error
+                messagebox.showwarning("Error", "La tabla no existe o esta vacia")
+                return None
+            else:
+                # Si el usuario existe se retorna el usuario encontrado
+                return usuario
+        # Si ocurre un error al consultar el usuario se muestra un mensaje de error
+        except sqlite3.OperationalError:
+            messagebox.showwarning("Error", "Ocurrio un error al consultar la tabla")
+
+    def actualizarUsuario(self, id, nombre, correo, contra):
+        # Buscar usuario en la base de datos por su id y actualizarlo
+        if (id == "" and correo == "" and contra == "" and nombre == ""):
+            messagebox.showwarning("CUIDADO", "Todos los campos estan vacios")
+            return
+        elif (id == "" or correo == "" or contra == "" or nombre == ""):
+            messagebox.showwarning("CUIDADO", "Revisa tus datos, uno o mas campos estan vacios")
+            return
+
+        #Validar que el id sea un numero
+        try:
+            id = int(id)
+        except ValueError:
+            messagebox.showwarning("CUIDADO", "El id debe ser un numero")
+            return
+
+        conx = self.conexionBD()
+        cursor = conx.cursor()
+
+        #Validar que el id exista
+        cursor.execute("select * from TBRegistros where id = ?", (id,))
+        usuario = cursor.fetchall()
+        if len(usuario) == 0:
+            messagebox.showwarning("CUIDADO", "El id no existe")
+            return
+        # Si el usuario existe se intenta actualizar
+        try:
+            # Encriptar contraseña
+            contra = self.encriptarContra(contra)
+            cursor.execute("UPDATE TBRegistros SET nombre = ?, correo = ?, contra = ? WHERE id = ?", (nombre, correo, contra, id))
+            conx.commit()
+            conx.close()
+            messagebox.showinfo("EXITO", "Se actualizo el usuario")
+        # Si ocurre un error al consultar el usuario se muestra un mensaje de error
+        except sqlite3.OperationalError:
+            messagebox.showwarning("Error", "Ocurrio un error al actualizar el usuario")
+
+    def eliminarUsuario(self, id):
+        # Buscar usuario en la base de datos por su id y eliminarlo
+        if (id == ""):
+            messagebox.showwarning("CUIDADO", "El campo id esta vacio")
+            return
+
+        #Validar que el id sea un numero
+        try:
+            id = int(id)
+        except ValueError:
+            messagebox.showwarning("CUIDADO", "El id debe ser un numero")
+            return
+
+        conx = self.conexionBD()
+        cursor = conx.cursor()
+
+        #Validar que el id exista
+        cursor.execute("select * from TBRegistros where id = ?", (id,))
+        usuario = cursor.fetchall()
+        if len(usuario) == 0:
+            messagebox.showwarning("CUIDADO", "El id no existe")
+            return
+        # Si el usuario existe se intenta eliminar
+        try:
+            cursor.execute("DELETE FROM TBRegistros WHERE id = ?", (id,))
+            conx.commit()
+            conx.close()
+            messagebox.showinfo("EXITO", "Se elimino el usuario")
+        # Si ocurre un error al consultar el usuario se muestra un mensaje de error
+        except sqlite3.OperationalError:
+            messagebox.showwarning("Error", "Ocurrio un error al eliminar el usuario")
+
+
